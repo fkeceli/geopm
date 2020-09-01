2360cddd6d58f3b1218dbe9ab085f6247bf7a191
Fuat Keceli <fuat.keceli@intel.com>
2020-08-31 19:31:29 -0700

    Added missing AED tests back to test/Makefile.mk

--- test/Makefile.mk
+++ test/Makefile.mk
@@ -153,6 +153,9 @@ GTEST_TESTS = test/gtest_links/AcceleratorTopoTest.default_config \
               test/gtest_links/EditDistEpochRecordFilterTest.pattern_add2 \
               test/gtest_links/EditDistEpochRecordFilterTest.pattern_subtract1 \
               test/gtest_links/EditDistEpochRecordFilterTest.fft_small \
+              test/gtest_links/EditDistEpochRecordFilterTest.sq_pattern_ab \
+              test/gtest_links/EditDistEpochRecordFilterTest.sq_pattern_abb \
+              test/gtest_links/EditDistEpochRecordFilterTest.sq_hacc_small \
               test/gtest_links/EditDistEpochRecordFilterTest.parse_name \
               test/gtest_links/EditDistPeriodicityDetectorTest.pattern_a \
               test/gtest_links/EditDistPeriodicityDetectorTest.pattern_ab \
@@ -164,6 +167,9 @@ GTEST_TESTS = test/gtest_links/AcceleratorTopoTest.default_config \
               test/gtest_links/EditDistPeriodicityDetectorTest.pattern_add2 \
               test/gtest_links/EditDistPeriodicityDetectorTest.pattern_subtract1 \
               test/gtest_links/EditDistPeriodicityDetectorTest.fft_small \
+              test/gtest_links/EditDistPeriodicityDetectorTest.sq_pattern_ab \
+              test/gtest_links/EditDistPeriodicityDetectorTest.sq_pattern_abb \
+              test/gtest_links/EditDistPeriodicityDetectorTest.sq_hacc_small \
               test/gtest_links/EndpointTest.attach_wait_loop_timeout_throws \
               test/gtest_links/EndpointTest.detach_wait_loop_timeout_throws \
               test/gtest_links/EndpointTest.get_hostnames \
